aa493b61fc27b1d34069a2919617927fcffd2d4f
Giovanni Squillace <giovanni.squillace.work@gmail.com>
2024-06-18 16:25:08 -0400

    added an underline effect to the nav bar and resized the hero

---
+++ src/shared/components/Nav-Bar/ContactBtn.jsx
@@ -0,0 +1,56 @@
+import { useEffect } from "react";
+import style from "./style.module.css";
+
+function startContactBtnShine() {
+	const contactAnimationSpeed = 600;
+
+	// Create the style element and before pseudo-class for the contact button
+	const ContactBeforeClass = document.createElement("style");
+	ContactBeforeClass.innerHTML = `
+	#contact-btn::before {
+	content: "";
+	position: absolute;
+	top: 0;
+	left: -100%;
+	width: 100%;
+	height: 100%;
+	background: linear-gradient(
+		120deg,
+		transparent,
+		var(--gold-color),
+		transparent
+	);
+	opacity: 0.35;
+
+	animation: shineAnimation ${String(contactAnimationSpeed)}ms forwards;
+	animation-play-state: running;
+	}`;
+
+	// add the class to the dom
+	document.head.appendChild(ContactBeforeClass);
+
+	// remove the pseudo-class in order to replay the animation
+	setTimeout(() => {
+		ContactBeforeClass.remove();
+	}, contactAnimationSpeed + 10);
+}
+
+export default function ContactBtn() {
+	useEffect(() => {
+		const contactButton = document.querySelector("#contact-btn");
+
+		contactButton.addEventListener("mouseover", () => {
+			startContactBtnShine();
+		});
+	}, []);
+
+	return (
+		<a
+			href="#"
+			id="contact-btn"
+			className={style.contact_navbar}
+		>
+			Contact
+		</a>
+	);
+}
--- src/shared/components/Nav-Bar/Nav-Bar.jsx
+++ src/shared/components/Nav-Bar/Nav-Bar.jsx
@@ -1,49 +1,7 @@
 import style from "./style.module.css";
-import { useEffect } from "react";
-
-function startContactBtnShine() {
-	const contactAnimationSpeed = 600;
-
-	// Create the style element and before pseudo-class for the contact button
-	const ContactBeforeClass = document.createElement("style");
-	ContactBeforeClass.innerHTML = `
-	#contact-btn::before {
-	content: "";
-	position: absolute;
-	top: 0;
-	left: -100%;
-	width: 100%;
-	height: 100%;
-	background: linear-gradient(
-		120deg,
-		transparent,
-		var(--gold-color),
-		transparent
-	);
-	opacity: 0.35;
-
-	animation: shineAnimation ${String(contactAnimationSpeed)}ms forwards;
-	animation-play-state: running;
-	}`;
-
-	// add the class to the dom
-	document.head.appendChild(ContactBeforeClass);
-
-	// remove the pseudo-class in order to replay the animation
-	setTimeout(() => {
-		ContactBeforeClass.remove();
-	}, contactAnimationSpeed + 10);
-}
+import ContactBtn from "./ContactBtn.jsx";
 
 export default function NavBar() {
-	useEffect(() => {
-		const contactButton = document.querySelector("#contact-btn");
-
-		contactButton.addEventListener("mouseover", () => {
-			startContactBtnShine();
-		});
-	}, []);
-
 	return (
 		<nav className={style.navBar_container}>
 			<a href="#">
@@ -55,13 +13,8 @@ export default function NavBar() {
 			<a href="#">
 				<span>03.</span> GitHub
 			</a>
-			<a
-				href="#"
-				id="contact-btn"
-				className={style.contact_navbar}
-			>
-				Contact
-			</a>
+
+			<ContactBtn />
 		</nav>
 	);
 }
